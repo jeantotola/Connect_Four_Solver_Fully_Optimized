0ddb0045b8307c34bd6242b70c72d4d605040f8a
Jean <jeantotola@hotmail.com>
2026-02-08 12:51:48 +0000

    Minimax with alpha-beta pruning implementation

--- Connect_N_Fully_Optimised.py
+++ Connect_N_Fully_Optimised.py
@@ -1,12 +1,17 @@
 import copy
-
+import math
 import numpy as np
 
 class ConnectN:
-    def __init__(self, rows=4, columns=5):
+    def __init__(self, rows=4, columns=5, win_condition=3):
         self.EMPTY = 0
         self.P1 = 1
         self.P2 = 2
+        self.DRAW = 'DRAW'
+        self.win_condition = win_condition
+
+        self.winner = None
+
         self.rows = rows
         self.columns = columns
 
@@ -15,23 +20,27 @@ class ConnectN:
 
         self.board = np.zeros((self.columns, self.rows), dtype=int)
 
-        self.winner = None
-
     def valid_move(self, col):
-        return -1 < col < self.columns and self.board[col][self.rows - 1] == self.EMPTY
+        return -1 < col < self.columns and self.board[col, self.rows - 1] == self.EMPTY
+
+    def actions(self):
+        # checks which columns have 0 at the top
+        # np.where returns a tuple, the first element of which is made into a list with .tolist()
+        return np.where(self.board[:, self.rows -1] == self.EMPTY)[0].tolist()
 
     def play(self, col):
         if not self.valid_move(col):
             raise ValueError("Column is either invalid or full")
 
         new_state = copy.deepcopy(self)
+
         for i in range(self.rows):
             if new_state.board[col][i] == self.EMPTY:
                 new_state.board[col][i] = new_state.next_player
-                if new_state.check_winner(col, i):
-                    new_state.winner = self.next_player
 
-                new_state.next_player = self.flip_player[self.next_player]
+                new_state.winner = new_state.check_winner(col, i)
+
+                new_state.next_player = self.flip_player[new_state.next_player]
 
                 return new_state
 
@@ -53,8 +62,8 @@ class ConnectN:
                 neighbour_c, neighbour_r = neighbour_c + axis[0], neighbour_r + axis[1]
 
             # early return
-            if total >= 3:
-                return True
+            if total >= self.win_condition:
+                return player
 
             neighbour_c, neighbour_r = original_c - axis[0], original_r - axis[1]
 
@@ -62,18 +71,54 @@ class ConnectN:
                 total += 1
                 neighbour_c, neighbour_r = neighbour_c - axis[0], neighbour_r - axis[1]
 
-            if total >= 3:
-                return True
+            if total >= self.win_condition:
+                return player
 
-        return False
+        if not self.actions():
+                return self.DRAW
+
+        return None
 
 class Minimax:
-    def next_move(self):
-        pass
-    def get_value(self):
-        pass
+    def next_move(self, state = ConnectN()):
+        player = state.next_player
+        best_action = None
+        best_value = -math.inf
+
+        for action in state.actions():
+            new_state = state.play(action)
+            new_value = self.get_value(new_state, player, get_min = True)
+            if new_value > best_value:
+                best_value = new_value
+                best_action = action
+
+        return best_action
+
+    def get_value(self, state, player, get_min, alpha = -math.inf, beta = math.inf):
+        other_player = state.flip_player[player]
+        winner = state.winner
+
+        if winner == player:
+            return 1
+        if winner == other_player:
+            return -1
+        if winner == state.DRAW:
+            return 0
+
+        for action in state.actions():
+            new_state = state.play(action)
+            action_value = self.get_value(new_state, player, get_min = not get_min, alpha = alpha, beta = beta)
+
+            if not get_min and action_value > alpha:
+                alpha = action_value
+
+            if get_min and action_value < beta:
+                beta = action_value
+
+            if alpha >= beta:
+                return alpha if not get_min else beta
+        return alpha if not get_min else beta
+
 
 exemplo = ConnectN()
-print (exemplo.board)
-novo = exemplo.play(1)
-print (novo.board)
+print (exemplo.play(2).board)
--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Beginning of a Connect Three solver.
 The idea is to make it a Minimax algorithm as optimised as gets:
 α-β Pruning, End-Game Tables, Simmetry Optimization, and Evaluation Functions.
 
-Until the moment, I have the game properly modelled and functional.
+Until the moment, I have the game properly modelled and functional and the minimax with alpha-beta pruning.
 
 Deepcopy will be replaced by an undo method. 
 
-Next step: simple Minimax algorithm
+Next step: depth and evaluate function
